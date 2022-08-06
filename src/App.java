919b5cf0947630ff873d1bd27bdc3629f112c55c
gustav0lopez <lopezmendozagustavo@gmail.com>
2022-08-06 20:19:41 -0300

    Java Básico 06082022

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        //Números
+
+        int edad=34;
+        double peso=83.9;
+        String nombre="Gustavo Lopez";
+        boolean esMayor=true;
+
+        System.out.println("Su nombre es :"+nombre+" su edad de "+edad+" su peso de "+peso+" y es mayor?" +esMayor);
+    }
+}
